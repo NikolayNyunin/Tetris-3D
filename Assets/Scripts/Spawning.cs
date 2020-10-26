a20e378332ec19adf7f2a7100d06bce4873f962f
NikolayNyunin <n.nyunin@mail.ru>
2020-10-26 21:07:47 +0300

    Added the ability to spawn new tetrominoes

---
+++ Assets/Scripts/Spawning.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject[] Tetrominoes;
+
+    void Start()
+    {
+        NewTetromino();
+    }
+
+    public void NewTetromino()
+    {
+        GameObject tetromino = Tetrominoes[Random.Range(0, Tetrominoes.Length)];
+        Instantiate(tetromino, transform.position, tetromino.transform.rotation);
+    }
+}
