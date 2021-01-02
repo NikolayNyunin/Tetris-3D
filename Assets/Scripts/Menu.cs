073b7722a8293838e5465f3cebe82941a28169e7
NikolayNyunin <n.nyunin@mail.ru>
2021-01-02 19:06:24 +0300

    Added main menu and basic settings menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayButtonPressed()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Toggle FullScreen;
+    public Dropdown Resolutions;
+
+    public GameObject MainMenu, SettingsMenu;
+
+    private bool fullScreen;
+    private List<string> resolutions;
+
+    void Start()
+    {
+        fullScreen = Screen.fullScreen;
+        FullScreen.isOn = fullScreen;
+
+        resolutions = new List<string>();
+
+        foreach (Resolution res in Screen.resolutions)
+            resolutions.Add(res.width + "x" + res.height + " " + res.refreshRate + "Hz");
+
+        Resolutions.ClearOptions();
+        Resolutions.AddOptions(resolutions);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+            BackButtonPressed();
+    }
+
+    public void FullScreenToggled(bool full)
+    {
+        fullScreen = full;
+        Screen.fullScreen = fullScreen;
+    }
+
+    public void ChangeResolution(int resIndex)
+    {
+        Resolution res = Screen.resolutions[resIndex];
+        Screen.SetResolution(res.width, res.height, fullScreen, res.refreshRate);
+    }
+
+    public void BackButtonPressed()
+    {
+        MainMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+}
